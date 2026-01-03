663e8f186d0cf1a2baf719ac838a979bff2f601d
Julian <schrieverjulian@gmail.com>
2026-01-03 16:42:21 +0100

    Update

--- src/de/api/devtools/MinecraftDevToolPlugin.java
+++ src/de/api/devtools/MinecraftDevToolPlugin.java
@@ -2,14 +2,13 @@ package de.api.devtools;
 
 import de.api.devtools.plugin.SpigotPlugin;
 import de.api.devtools.utils.TextColor;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 public final class MinecraftDevToolPlugin extends SpigotPlugin {
 
     @Override
     public void onPluginStart() {
-        Bukkit.getConsoleSender().sendMessage(getPrefix() + ChatColor.GREEN + "Successfully Enabled :D");
+        getConsole().sendMessage(getPrefix() + ChatColor.GREEN + "Successfully Enabled :D");
     }
 
     @Override
